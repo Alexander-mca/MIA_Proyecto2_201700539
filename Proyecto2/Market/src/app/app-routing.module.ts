7c998c9d96041c2f69ca53d5017f8376e97b35ae
Alex <denisalexandermorales@gmail.com>
2020-10-24 00:22:44 -0600

    Commit 2

--- Proyecto2/Market/src/app/app-routing.module.ts
+++ Proyecto2/Market/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { AppComponent } from './app.component';
+import { InicioComponent } from './Components/inicio/inicio.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistroComponent } from './Components/Registro/Registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'registro',
+  component:RegistroComponent,
+},{
+  path:'login',
+  component:InicioComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
